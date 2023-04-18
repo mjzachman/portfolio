562e1a6e89289e59204df64bc97246f303b50345
Matt Zachman <mzachman14@gmail.com>
2023-04-18 19:02:20 -0400

    finish welcome page

--- src/Restaurant/components/Welcome.js
+++ src/Restaurant/components/Welcome.js
@@ -9,12 +9,13 @@ function Welcome() {
 
     return <div className="Welcome">
             <h1 id="welcome-header">Welcome to the Margarita Shop</h1>
+            
+            <div id="welcome-content">
             <img className="margarita" id="red" src={red} alt="a red margarita" />
             <img className="margarita" id="yellow" src={yellow} alt="a yellow margarita" />
             <img className="margarita" id="green" src={green} alt="a green margarita" />
-            <div id="welcome-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p></div>
             <a href="/restaurant/about"><p>About Us</p></a>
             
         </div>
